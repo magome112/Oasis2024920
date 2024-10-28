4f48dd7103940c629f5895aef55ccc65a3075e1c
magome112 <2401112@s.asojuku.ac.jp>
2024-10-28 01:54:00 +0000

    テスト

---
+++ newuser.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>アカウント作成</title>
+</head>
+<body>
+    <input type="radio" name="new">アカウント新規作成>
+    <p>メールアドレスまたは電話番号</p>
+    <input type="text" name="mail">
+    <p>新しいパスワードを入力</p>
+    <input type="text" name="password">
+
+    <input type="submit" value="新規作成">
+
+    <input type="radio" name="gesugu"
+</body>
+</html>
