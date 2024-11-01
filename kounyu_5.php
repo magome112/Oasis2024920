5796de9ce9631b84cb257fa8658cf1563af56d86
2301037 <2301037@s.asojuku.ac.jp>
2024-11-01 12:24:27 +0900

    購入情報画面の変更

---
+++ kounyu_5.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>購入情報画面</title>
+</head>
+<body>
+<fieldset>
+    <h3>1.購入者情報</h3>
+    国/地域
+    <select name="purchaser_country" >
+        <option value = "japan" selected>Japan</option>
+    </select>
+    
+</fieldset>
+</body>
+</html>
