f016b769964f1492dc9e3b1bae9e284a6408f4fb
2301037 <2301037@s.asojuku.ac.jp>
2024-11-01 12:56:39 +0900

    購入情報画面の追加

--- kounyu_5.php
+++ kounyu_5.php
@@ -9,9 +9,30 @@
 <fieldset>
     <h3>1.購入者情報</h3>
     国/地域
+    <br>
     <select name="purchaser_country" >
         <option value = "japan" selected>Japan</option>
+        <option value = "US">America</option>
+        <option value = "UK">United Kingdom</option>
     </select>
+    氏名
+    <br>
+    <input type="text" name="purchaser_name">
+    <br>
+    住所
+    <br>
+    <input type="text" name="u_address">
+    <br>
+    電話番号
+    <br>
+    <input type="text" name="tell">
+
+    <h3>2.お支払方法</h3>
+
+    <input type="radio" name="payment" value="visa">VISAの画像
+    <input type="radio" name="payment" value="jcb">JCBの画像
+    <input type="radio" name="payment" value="paypay">paypayの画像
+    <input type="radio" name="payment" value="R_pay">R_payの画像
     
 </fieldset>
 </body>
