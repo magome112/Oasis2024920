2e9830b3e89a52d44f2cea1d694841649223bbe9
magome112 <2401112@s.asojuku.ac.jp>
2024-11-01 12:26:27 +0900

    作成

---
+++ roguin_2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+</head>
+<body>
+    <img src="">
+    <hr>
+
+    <input type="radio" name="login" value="ログイン">
+    <p>メールアドレスまたは電話番号</p>
+    <input type="text" name="mail">
+
+    <p>パスワードを入力</p>
+    <input type="text" name="pass">
+
+    <input type="radio" name="guestlog" value="ゲストでログイン">
+    <input type="radio" name="newuser" value="アカウント新規作成">
+    <a href="./kanri.roguin_10.php">管理者ログイン</a>
+</body>
+</html>
