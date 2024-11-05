13939e6fa12a829812ae1902d6fe08fc2e3dd2c3
magome112 <2401112@s.asojuku.ac.jp>
2024-11-05 12:06:40 +0900

    変更

--- newuser_1.php
+++ newuser_1.php
@@ -6,18 +6,20 @@
     <title>アカウント作成</title>
 </head>
 <body>
-    <img src="">
-    <hr>
-    
-    <input type="radio" name="new" value="アカウント新規作成">
-    <p>メールアドレスまたは電話番号</p>
-    <input type="text" name="mail">
-    <p>新しいパスワードを入力</p>
-    <input type="text" name="password">
+    <form action="./home_3.php" method="post">
+        <img src="">
+        <hr>
+        
+        <input type="radio" name="new" value="アカウント新規作成">
+        <p>メールアドレスまたは電話番号</p>
+        <input type="text" name="mail">
+        <p>新しいパスワードを入力</p>
+        <input type="text" name="password">
 
-    <input type="submit" value="新規作成">
+        <input type="submit" value="新規作成">
 
-    <input type="radio" name="guest" value="ゲストでログイン">
-    <input type="radio" name="roguin" value="ログイン">
+        <input type="radio" name="guest" value="ゲストでログイン">
+        <input type="radio" name="roguin" value="ログイン">
+    </form>
 </body>
 </html>
