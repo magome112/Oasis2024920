0594e5719cf6316403b3e982a4a3ffe2b9ae14c4
magome112 <2401112@s.asojuku.ac.jp>
2024-11-08 12:32:20 +0900

    データベース接続

--- kanri.add shohin_15.php
+++ kanri.add shohin_15.php
@@ -1,1 +1,12 @@
-q
+<?php
+// データベース接続情報
+$host = 'localhost';
+$dbname = 'ecommerce';
+$username = 'LAA1553845';
+$password = 'pass1234';
+
+
+    // データベースに接続
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
