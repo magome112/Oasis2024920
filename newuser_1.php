421ba382ce1083054c1df13df7325b3eeaa707dc
magome112 <2401112@s.asojuku.ac.jp>
2024-11-08 13:37:35 +0900

    修正

--- newuser_1.php
+++ newuser_1.php
@@ -6,15 +6,18 @@
     <title>アカウント作成</title>
 </head>
 <body>
-    <input type="radio" name="new">アカウント新規作成>
+    <img src="./images/oasislogo.jpg" width="100" height="50">
+    <hr>
+    <input type="radio" name="new">アカウント新規作成
     <p>メールアドレスまたは電話番号</p>
     <input type="text" name="mail">
     <p>新しいパスワードを入力</p>
     <input type="text" name="password">
 
-    <input type="submit" value="新規作成">
+    <p><input type="submit" value="新規作成"></p>
 
-    <input type="radio" name="guest">
+    <input type="radio" name="guest">ゲストでログイン
+    <input type="radio" name="login">ログイン
     
 </body>
 </html>
