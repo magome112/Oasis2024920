094c993f9efa3fdd28e385dc5a3bb3bb7045a2a7
magome112 <2401112@s.asojuku.ac.jp>
2024-11-08 14:47:33 +0900

    cssとphpのdiv要素を追加

--- home_3.php
+++ home_3.php
@@ -3,23 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/stylesheet_3.css">
     <title>ホーム</title>
 </head>
 <body>
     <form action="./shohin_4.php" method="post">
         <input type="search" name="search">
-        <img src="./images/oasislogo.jpg" width="200" height="100">
+        <img src="./images/oasislogo.jpg" width="100" height="50">
         <hr>
 
         <h2>海外</h2>
-        <img src="./images/eberesuto.jpg" width="200" height="100"><br>エベレスト
-        <img src="./images/kannchenjunnga.jpg" width="200" height="100"><br>カンチェンジュンガ
-        <img src="./images/manasuru.jpg" width="200" height="100"><br>マナスル
+        <div class="img-side">
+            <img src="./images/eberesuto.jpg" width="200" height="100"><br>エベレスト
+            <img src="./images/kannchenjunnga.jpg" width="200" height="100"><br>カンチェンジュンガ
+            <img src="./images/manasuru.jpg" width="200" height="100"><br>マナスル
+        </div>
 
         <h2>国内</h2>
-        <img src="./images/hujisan.jpg" width="200" height="100"><br>富士山
-        <img src="./images/hodaka.jpg" width="200" height="100"><br>穂高岳
-        <img src="./images/sakurajima.jpg" width="200" height="100"><br>桜島
+        <div class="img-side">
+            <img src="./images/hujisan.jpg" width="200" height="100"><br>富士山
+            <img src="./images/hodaka.jpg" width="200" height="100"><br>穂高岳
+            <img src="./images/sakurajima.jpg" width="200" height="100"><br>桜島
+        </div>
 
         <img src="./images/oasislogo.jpg" width="200" height="100">
         <a href="./kounyurireki_8.php">購入履歴</a>
