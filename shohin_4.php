d5f628cf24b8604d7ac8c3a7f25c63164a34e17c
2301037 <2301037@s.asojuku.ac.jp>
2024-11-08 12:21:45 +0900

    ４と５の軽微な変更、接続設定11/08

--- shohin_4.php
+++ shohin_4.php
@@ -6,6 +6,16 @@
     <title>商品詳細画面</title>
 </head>
 <body>
+    <form action="kounyu_5.php" method="post">
+        <p><input type="submit" value="購入する"></p>
+    </form>
 
+    <form action="rentaru_9.php" method="post">
+        <p><input type="submit" value="レンタルする"></p>
+    </form>
+
+    <form action="review_7.php" method="post">
+        <p><input type="submit" value="レビューを見る"></p>
+    </form>
 </body>
 </html>
