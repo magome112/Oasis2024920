53a408ff5c297b2146c1ce64898dc20c54e9cd83
magome112 <2401112@s.asojuku.ac.jp>
2024-11-08 13:59:16 +0900

    css,shohin_4はデータベース接続を削除

--- newuser_1.php
+++ newuser_1.php
@@ -3,22 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/stylesheet_1.css">
     <title>アカウント作成</title>
 </head>
 <body>
-    <img src="./images/oasislogo.jpg" width="100" height="50">
-    <hr>
-    <input type="radio" name="new">アカウント新規作成
-    <p>メールアドレスまたは電話番号</p>
-    <input type="text" name="mail">
-    <p>新しいパスワードを入力</p>
-    <input type="text" name="password">
+    <form action="./home_3.php" method="post">
+        <img src="./images/oasislogo.jpg" id="center-img" width="100" height="50">
+        <hr>
+        <input type="radio" name="new" value="new" class="radio">アカウント新規作成
 
-    <p><input type="submit" value="新規作成"></p>
+            <p class="p">メールアドレスまたは電話番号</p>
+            <input type="text" name="mail" class="textbox">
 
-    <input type="radio" name="guest">ゲストでログイン
-    <input type="radio" name="login">ログイン
+            <p class="p">新しいパスワードを入力</p>
+            <input type="text" name="password" class="textbox">
+
+        <p class="p"><input type="submit" value="新規作成"></p>
+
+        <input type="radio" name="guest" value="guest" class="radio">ゲストでログイン
+        <input type="radio" name="login" value="login" class="radio">ログイン
     
-    
+    </form>
 </body>
 </html>
