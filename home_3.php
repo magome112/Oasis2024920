f6cba7aa709b530bd9e2b134d9008a6e9f367919
magome112 <2401112@s.asojuku.ac.jp>
2024-11-11 09:46:24 +0900

    画像の追加、imgタグの追加

--- home_3.php
+++ home_3.php
@@ -17,15 +17,16 @@
             <div class="img-item">
                 <img src="./images/eberesuto.jpg" width="200" height="100">
                 <p>エベレスト</p>
-            </div>
-            <div class="img-item">
                 <img src="./images/kannchenjunnga.jpg" width="200" height="100">
                 <p>カンチェンジュンガ</p>
-            </div>
-            <div class="img-item">
                 <img src="./images/manasuru.jpg" width="200" height="100">
                 <p>マナスル</p>
+                <img src="./images/mattahorun.jpg" width="200" height="100">
+                <p>マッターホルン</p>
+                <img src="./images/kirimanjaro.jpg" width="200" height="100">
+                <p>キリマンジャロ</p>
             </div>
+
         </div>
 
         <h2>国内</h2>
@@ -33,14 +34,14 @@
             <div class="img-item">
                 <img src="./images/hujisan.jpg" width="200" height="100">
                 <p>富士山</p>
-            </div>
-            <div class="img-item">
                 <img src="./images/hodaka.jpg" width="200" height="100">
                 <p>穂高岳</p>
-            </div>
-            <div class="img-item">
                 <img src="./images/sakurajima.jpg" width="200" height="100">
                 <p>桜島</p>
+                <img src="./images/asozan.jpg" width="200" height="100">
+                <p>阿蘇山</p>
+                <img src="./images/rijirizan.jpg" width="200" height="100">
+                <p>利尻山</p>
             </div>
         </div>
 
