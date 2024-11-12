50b36b1fca24ce9a497628c6b10d40b502418e90
yo-ta0016 <2301012@s.asojuku.ac.jp>
2024-11-12 12:37:03 +0900

    テスト

--- _11_kanri.home.php
+++ _11_kanri.home.php
@@ -40,22 +40,4 @@
 $dsn = 'mysql:host=mysql306.phy.lolipop.lan;dbname=LAA1602729-oasis;charset=utf8mb4';  // DSN（データソース名）
 $username = 'LAA1602729';  // ユーザー名
 $password = 'oasis5';  // パスワード
-
-try {
-  // PDOインスタンスを作成してデータベースに接続
-  $pdo = new PDO($dsn, $username, $password);
-  
-  // エラーモードを設定（例外を投げる設定）
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  // 接続成功のメッセージを表示
-  echo "データベースに接続しました！";
-  
-} catch (PDOException $e) {
-  // 接続失敗時のエラーメッセージを表示
-  echo "接続失敗: " . $e->getMessage();
-}
 ?>
-
-</body>
-</html> 
