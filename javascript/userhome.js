de4867c8735352279d1f02aa80b5a173aa7f10eb
magome112 <2401112@s.asojuku.ac.jp>
2024-11-15 12:44:09 +0900

    css js修正

--- javascript/userhome.js
+++ javascript/userhome.js
@@ -1,10 +1,19 @@
+let currentIndex = 0;
+
 function slideImages(containerId) {
     const container = document.getElementById(containerId);
     const slideWidth = container.querySelector('.img-slide').offsetWidth + 15;
 
-    //スクロールを行う
+    //次のスライド位置へスクロール
     container.scrollBy({
        left: slideWidth,
        behavior: 'smooth' 
     });
+
+    //最後まで行ったら最初に戻す
+    currentIndex += 1;
+    if(currentIndex >= container.children.length - 5) {
+        currentIndex = 0;
+        container.scrollTo({ left: 0, behavior: 'smooth' });
+    }
 }
