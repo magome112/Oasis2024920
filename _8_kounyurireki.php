0cca67e8f790f5696a5702f0626e3108eb21cc41
Fukigen-na-Kijitora <2301032@s.asojuku.ac.jp>
2024-11-15 14:43:20 +0900

    Update _8_kounyurireki.php

--- _8_kounyurireki.php
+++ _8_kounyurireki.php
@@ -1,6 +1,3 @@
-
-
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -10,16 +7,6 @@
 </head>
 <body>
 <?php
-/*データベース接続情報
-$host = 'localhost';
-$dbname = 'ecommerce';
-$username = 'LAA1553845';
-$password = 'pass1234';
-
-
-    // データベースに接続
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);*/
     $pdo = new PDO('mysql:host=mysql306.phy.lolipop.lan;
                     dbname=LAA1602729-oasis;charset=utf8',
                     'LAA1602729',
