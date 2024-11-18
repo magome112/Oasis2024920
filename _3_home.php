f76ded25b856929194864950a1ca2a1aa8df4924
magome112 <2401112@s.asojuku.ac.jp>
2024-11-18 11:21:57 +0900

    行削除

--- _3_home.php
+++ _3_home.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="jp">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/stylesheet_3.css">
+    <link rel="icon" href="/favicon.ico" />
     <title>ホーム</title>
 </head>
 <body>
