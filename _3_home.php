852a5bf4fb45fbe0aaef82a6d4649c11de6e9d83
magome112 <2401112@s.asojuku.ac.jp>
2024-11-18 10:12:26 +0900

    php修正

--- _3_home.php
+++ _3_home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/stylesheet_3.css">
+    <link rel="stylesheet" href="./css/stylesheet_3.css">
     <title>ホーム</title>
 </head>
 <body>
@@ -63,6 +63,6 @@
     }
 ?>
 
-<script src="/javascript/userhome.js"></script>
+<script src="./javascript/userhome.js"></script>
 </body>
 </html>
