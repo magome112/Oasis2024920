c4fa852be93f1c11b787b3a28c27748ebd0f100b
Marutya1219 <2301028@s.asojuku.ac.jp>
2024-11-19 11:42:14 +0900

    変更

--- _8_kounyurireki.php
+++ _8_kounyurireki.php
@@ -7,9 +7,18 @@ $redirectUrl = "https://aso2301032.girlfriend.jp/Oasis2024920/_3_home.php";
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/stylesheet_3.css">
+    <link rel="icon" href="/favicon.ico" />
     <title>購入履歴</title>
+</head>
+
+    <div class="header-img">
+        <input type="search" name="search">
+        <img src="./images/oasislogo.jpg" width="100" height="50">
+    </div>
+    <hr>
+
 
     <?php
     $pdo = new PDO('mysql:host=mysql306.phy.lolipop.lan;
