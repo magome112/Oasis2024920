cd9bb558e23fe46fe6515f4b089d8b4b9d80d6cc
Fukigen-na-Kijitora <2301032@s.asojuku.ac.jp>
2024-11-19 12:37:48 +0900

    様子見

--- _7_review.php
+++ _7_review.php
@@ -6,13 +6,17 @@
     <title>Document</title>
 </head>
 <body>
-    
-</body>
-</html>
 <?php
     $pdo = new PDO('mysql:host=mysql306.phy.lolipop.lan;
                     dbname=LAA1602729-oasis;charset=utf8',
                     'LAA1602729',
                     'oasis5');
 
+    $sql1 = "SELECT `yama_img`, `yama_name` FROM `Oasis_yama` WHERE `Region` = 1 ";
+    $result1 = $pdo->query($sql1);
+    var_dump($result1->fetchAll());
+    $rowCount = $result1->rowCount();
+                
 ?>
+</body>
+</html>
