0bb69d42e27e46d6877f7794eb95f97cf23c216f
magome112 <2401112@s.asojuku.ac.jp>
2024-11-22 11:41:55 +0900

    修正

--- _4_shohin.php
+++ _4_shohin.php
@@ -18,6 +18,30 @@
                     'LAA1602729',
                     'oasis5');
 
+    // yama_nameを使って、同レコードの情報を取得
+    $sql = "SELECT * FROM Oasis_yama WHERE yama_name = :yama_name";
+    // データベースから取得した情報を表示
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':yama_name', $yama_name, PDO::PARAM_STR);
+    $stmt->execute();
+
+    // 結果を取得
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);  // 1件の結果を連想配列として取得
+
+    echo $result['yama_img']. 'id="body_img"';
+    echo '<div class="body_text">';
+    echo $result['yama_name'];
+    echo $result['price'];
+    echo '</div>';
+    echo $result['yama_info']. 'id="mt_info"';
+
+    // 他のカラムを利用
+    echo "山の名前: " . $result['yama_name'];
+    echo "山の画像: " . $result['yama_img'];
+    echo "山のID: " . $result['yama_id'];
+
+
+
 ?>
 </body>
 </html>
