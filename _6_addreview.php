a463ff27f7e61b1c6f6fa2e90b77b10dd89edcda
Fukigen-na-Kijitora <2301032@s.asojuku.ac.jp>
2024-11-22 14:57:17 +0900

    6

--- _6_addreview.php
+++ _6_addreview.php
@@ -1,11 +1,3 @@
-<?php
-    $pdo = new PDO('mysql:host=mysql306.phy.lolipop.lan;
-                    dbname=LAA1602729-oasis;charset=utf8',
-                    'LAA1602729',
-                    'oasis5');
-
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -13,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>レビュー記入</title>
 </head>
+
 <body>
 <div class="header-img">
         <input type="search" name="search">
@@ -29,10 +22,35 @@
                     <input type="radio" name="star" value="1"></p>
 
     <p><h5>コメント(必須)</h5> <textarea rows="5" cols="50"></textarea></p>
-
     <h5>画像アップロード(任意)</h5><input type="file" name="upload" accept="image/*">
+
     </form>
-    <button></button>
+    <?php
+$pdo = new PDO('mysql:host=mysql306.phy.lolipop.lan;
+                dbname=LAA1602729-oasis;charset=utf8',
+                'LAA1602729',
+                'oasis5');
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    
+    $sql = "INSERT INTO Oasis_review (review_id, email) VALUES (:username, :email)";
+    $stmt = $conn->prepare($sql);
+
+    // フォームデータをバインドして実行
+    $stmt->bindParam(':username', $user_name);
+    $stmt->bindParam(':email', $user_email);
+    $stmt->execute();
+
+    if ($stmt->execute()) {
+        echo "データが正常に保存されました！";
+    } else {
+        echo "エラー: " . $stmt->error;
+    }
+
+// 接続を閉じる
+$conn = null;
+?>
 
 </body>
 </html>
