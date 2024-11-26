136c26559a4bb1b4fa5241db79507da9ecacc6be
magome112 <2401112@s.asojuku.ac.jp>
2024-11-26 12:36:50 +0900

    修正

--- _15_kanri.add.shohin.php
+++ _15_kanri.add.shohin.php
@@ -16,28 +16,41 @@
 </head>
 <body>
     <h2>商品追加</h2>
-        <form action="./_12_kanri.shohin.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="product_img[]" id="product_img" multiple accept="image/*">
-            <br>
-            <label for="country_category" id="country_category">国/地域
-                <select name="country">
-                    <option value="日本">日本</option>
-                    <option value="アメリカ">アメリカ</option>
-                    <option value="イギリス">イギリス</option>
-                    <option value="カナダ">カナダ</option>
-                    <option value="オーストラリア">オーストラリア</option>
-                </select></label>
-                <br>
-                <label class="form_data">山名
-                    <input type="text" name="country_name"></label>
-                <br>
-                <label class="form_data">金額
-                    <input type="text" name="price"></label>
-                <br>
-                <label class="form_data">詳細
-                    <textarea name="detail" rows="4" cols="30"></textarea></label>
-                
-                    <input type="submit" value="商品を追加する">
-        </form>
+        <div class="container">
+            <form action="./_12_kanri.shohin.php" method="post" enctype="multipart/form-data">
+                <div class="img_section">
+                    <label for="product_img" class="img_placeholder">
+                        <input type="file" name="product_img[]" id="product_img" multiple accept="image/*" hidden>
+                        <img src="placeholder-icon.png" alt="イメージ" class="placeholder-icon">
+                    </label>
+                    <div class="thumbnail">
+                        <div class="thumbnail">+</div>
+                        <div class="thumbnail">+</div>
+                        <div class="thumbnail">+</div>
+                    </div>
+                </div>
+                <div class="form_section">
+                    <label for="country_category">国/地域
+                        <select name="country">
+                            <option value="日本">日本</option>
+                            <option value="アメリカ">アメリカ</option>
+                            <option value="イギリス">イギリス</option>
+                            <option value="カナダ">カナダ</option>
+                            <option value="オーストラリア">オーストラリア</option>
+                        </select>
+                    </label>
+                    <label for="country_name">山名
+                        <input type="text" name="country_name" placeholder="例：富士山">
+                    </label>
+                    <label for="country_price">金額
+                        <input type="text" name="country_price" placeholder="100,000,000">
+                    </label>
+                    <label for="country_detail">詳細
+                        <textarea rows="4" cols="30" name="country_detail" placeholder="日本最高峰の山">
+                    </label>
+                </div>
+                <button type="submit" class="submit_btn">商品を追加する</button>
+            </form>
+        </div>
 </body>
 </html>
