67a117e7300d1431756637436226789db98ce644
magome112 <2401112@s.asojuku.ac.jp>
2024-11-29 10:22:07 +0900

    修正

--- _15_kanri.add.shohin.php
+++ _15_kanri.add.shohin.php
@@ -1,11 +1,9 @@
 <?php
-        $pdo = new PDO('mysql:host=mysql306.phy.lolipop.lan;
-        dbname=LAA1602729-oasis;charset=utf8',
-        'LAA1602729',
-        'oasis5');
-
-    
-    ?>
+$pdo = new PDO('mysql:host=mysql306.phy.lolipop.lan;
+    dbname=LAA1602729-oasis;charset=utf8',
+    'LAA1602729',
+    'oasis5');
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -16,41 +14,40 @@
 </head>
 <body>
     <h2>商品追加</h2>
-        <div class="container">
-            <form action="./_12_kanri.shohin.php" method="post" enctype="multipart/form-data">
-                <div class="img_section">
-                    <label for="product_img" class="img_placeholder">
-                        <input type="file" name="product_img[]" id="product_img" multiple accept="image/*" hidden>
-                        <img src="placeholder-icon.png" alt="イメージ" class="placeholder-icon">
-                    </label>
-                    <div class="thumbnail">
-                        <div class="thumbnail">+</div>
-                        <div class="thumbnail">+</div>
-                        <div class="thumbnail">+</div>
-                    </div>
+    <div class="container">
+        <form action="./_12_kanri.shohin.php" method="post" enctype="multipart/form-data">
+            <div class="img_section">
+                <label for="product_img" class="img_placeholder">
+                    <input type="file" name="product_img[]" id="product_img" multiple accept="image/*" hidden>
+                    <img src="placeholder-icon.png" alt="イメージ" class="placeholder-icon" id="preview">
+                </label>
+                <div class="thumbnail-container" id="thumbnails">
+                    <!-- サムネイルがここに表示される -->
                 </div>
-                <div class="form_section">
-                    <label for="country_category">国/地域
-                        <select name="country">
-                            <option value="日本">日本</option>
-                            <option value="アメリカ">アメリカ</option>
-                            <option value="イギリス">イギリス</option>
-                            <option value="カナダ">カナダ</option>
-                            <option value="オーストラリア">オーストラリア</option>
-                        </select>
-                    </label>
-                    <label for="country_name">山名
-                        <input type="text" name="country_name" placeholder="例：富士山">
-                    </label>
-                    <label for="country_price">金額
-                        <input type="text" name="country_price" placeholder="100,000,000">
-                    </label>
-                    <label for="country_detail">詳細
-                        <textarea rows="4" cols="30" name="country_detail" placeholder="日本最高峰の山"></textarea>
-                    </label>
-                </div>
-                <button type="submit" class="submit_btn">商品を追加する</button>
-            </form>
-        </div>
+            </div>
+            <div class="form_section">
+                <label for="country_category">国/地域
+                    <select name="country">
+                        <option value="日本">日本</option>
+                        <option value="アメリカ">アメリカ</option>
+                        <option value="イギリス">イギリス</option>
+                        <option value="カナダ">カナダ</option>
+                        <option value="オーストラリア">オーストラリア</option>
+                    </select>
+                </label>
+                <label for="country_name">山名
+                    <input type="text" name="country_name" placeholder="例：富士山">
+                </label>
+                <label for="country_price">金額
+                    <input type="text" name="country_price" placeholder="例：100,000,000">
+                </label>
+                <label for="country_detail">詳細
+                    <textarea rows="4" cols="30" name="country_detail" placeholder="日本最高峰の山"></textarea>
+                </label>
+            </div>
+            <button type="submit" class="submit_btn">商品を追加する</button>
+        </form>
+    </div>
+    <script src="./js/_15_script.js"></script>
 </body>
 </html>
