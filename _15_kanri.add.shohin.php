6bf10693ea9383384ffc1f473a679164250b7438
magome112 <2401112@s.asojuku.ac.jp>
2024-12-03 11:35:49 +0900

    css修正

--- _15_kanri.add.shohin.php
+++ _15_kanri.add.shohin.php
@@ -51,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/stylesheet_3.css">
+    <link rel="stylesheet" href="./css/stylesheet_sidebar.css">
     <title>商品追加</title>
 </head>
 <body>
